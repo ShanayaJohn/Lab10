9e153fc8f6b62c1803640dea063f0e0f616feb2c
Shanaya John <shanaya.john@ryerson.ca>
2022-12-03 01:40:26 -0500

    yes

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ function App() {
     <div className="App">
       <h1>Hello World</h1>
       <h2>Installation</h2>
-      <p>This was a complex process to understand. To install and run react on my computer, I had to install node.js.</p>
+      <p>This was a complex process to understand. To install and run react on my computer, I had to install node.js and git.</p>
       <h2>Building</h2>
-      <p>To create this website, used the vscode with the command: "upx create-react-app name". 
-        After multiple installing, I was given a template which I edited to create this website. 
+      <p>To create this website, I used the vscode with the command: "upx create-react-app name". 
+        After multiple installation, I was given a template which I edited to create this website. 
         I used basic html tag to create the "hello world" heading and this explanation. </p>
       <h2> Challenges</h2>
-      <p>The main challenge was understand the installation process and how to host my website on a free server. 
+      <p>The main challenge was understand the installation process and how to host my website on a free server. I had a hard time understanding how to upload my work to github and link it to the vscode on my computer.
         To solve this I consulted alot of Youtube videos. </p>
     </div>
   );
