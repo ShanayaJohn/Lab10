478514f3d354eb11f5f731febe022c9535831134
Shanaya John <shanaya.john@ryerson.ca>
2022-12-03 01:31:29 -0500

    yes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <h2>Installation</h2>
+      <p>This was a complex process to understand. To install and run react on my computer, I had to install node.js.</p>
+      <h2>Building</h2>
+      <p>To create this website, used the vscode with the command: "upx create-react-app name". 
+        After multiple installing, I was given a template which I edited to create this website. 
+        I used basic html tag to create the "hello world" heading and this explanation. </p>
+      <h2> Challenges</h2>
+      <p>The main challenge was understand the installation process and how to host my website on a free server. 
+        To solve this I consulted alot of Youtube videos. </p>
     </div>
   );
 }
